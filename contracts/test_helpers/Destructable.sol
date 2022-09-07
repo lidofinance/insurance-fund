4c869e2ca497f235d6a43584d7ad6a81764993e4
Azat Serikov <azatsdev@gmail.com>
2022-09-07 13:40:20 +0600

    feat: test sender ether to insurance fund

---
+++ contracts/test_helpers/Destructable.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.16;
+
+/// @title Destructable
+/// @author info@lido.fi
+/// @notice a test contract for transferring ether to non-payable contracts
+contract Destructable {
+
+    /// @notice destroys the contact and forwards ether to another addres
+    /// @dev force ether onto another non-payable contract
+    /// @param _heir address to forward ether to
+    function die(address _heir) public payable {
+        selfdestruct(payable(_heir));
+    }
+}
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 import brownie
+import eth_utils
 
 
 @pytest.fixture(autouse=True)
@@ -17,12 +18,24 @@ def stranger(accounts):
     return accounts[1]
 
 
-# @pytest.fixture(scope="session", params=["owner", "stranger"])
-# def sender(request):
-#     return request.getfixturevalue(request.param)
+@pytest.fixture(scope="session", params=["owner", "stranger"])
+def anyone(request):
+    return request.getfixturevalue(request.param)
 
 
 @pytest.fixture(scope="function")
 def insurance_fund(owner):
     return brownie.InsuranceFund.deploy(owner.address, {"from": owner})
 
+
+@pytest.fixture(scope="function")
+def destructable(anyone):
+    return brownie.Destructable.deploy({"from": anyone})
+
+
+@pytest.fixture(
+    scope="session",
+    params=[0, 1, eth_utils.to_wei(1, "gwei"), eth_utils.to_wei(1, "ether")],
+)
+def ether_amount(request):
+    return request.param
