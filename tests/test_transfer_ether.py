4dfdcc7ae5955cd7ae9a1657afe46933fb6f896f
Azat Serikov <azatsdev@gmail.com>
2022-09-09 11:59:09 +0600

    refactor: group ether tests

--- tests/test_transfer_ether.py
+++ tests/test_transfer_ether.py
@@ -1,6 +1,43 @@
 import brownie
 
 
+def test_receive_ether(insurance_fund, anyone, ether_amount):
+    prev_insurance_fund_balance = insurance_fund.balance()
+    prev_anyone_balance = anyone.balance()
+
+    revert_string = (
+        "" if ether_amount == 0 else "Cannot send ether to nonpayable function"
+    )
+    with brownie.reverts(revert_string):
+        anyone.transfer(insurance_fund.address, ether_amount)
+
+    assert (
+        insurance_fund.balance() == prev_insurance_fund_balance
+    ), "insurance fund balance should remain unchanged"
+    assert (
+        anyone.balance() == prev_anyone_balance
+    ), "sender balance should remain unchanged"
+
+
+def test_receive_ether_via_selfdestruct(
+    insurance_fund, destructable, anyone, ether_amount
+):
+    prev_insurance_fund_balance = insurance_fund.balance()
+    prev_anyone_balance = anyone.balance()
+
+    tx = destructable.die(
+        insurance_fund.address, {"from": anyone, "value": ether_amount}
+    )
+    tx_fee = tx.gas_used * tx.gas_price
+
+    assert (
+        insurance_fund.balance() == prev_insurance_fund_balance + ether_amount
+    ), "insurance fund should receive ether"
+    assert (
+        anyone.balance() == prev_anyone_balance - ether_amount - tx_fee
+    ), "sender balance should be less by amount of ether sent plus gas fees"
+
+
 def test_transfer_ether_as_stranger(
     insurance_fund, destructable, stranger, ether_amount
 ):
