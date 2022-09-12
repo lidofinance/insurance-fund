e54bddc3902e8fe1a9913c24f4987f21f55a67c8
Azat Serikov <azatsdev@gmail.com>
2022-09-12 13:37:40 +0600

    feat: improve comments

--- contracts/InsuranceFund.sol
+++ contracts/InsuranceFund.sol
@@ -10,7 +10,7 @@ import "./dependencies/OpenZeppelin/token/ERC1155/IERC1155.sol";
 
 /// @title Lido Insurance Fund
 /// @author mymphe
-/// @notice serves as a vault for Lido insurance funds; `owner` has full access
+/// @notice serves as a vault for Lido insurance funds
 contract InsuranceFund is Ownable {
     using SafeERC20 for IERC20;
 
@@ -38,22 +38,22 @@ contract InsuranceFund is Ownable {
         transferOwnership(_owner);
     }
 
-    /// @notice prevents burn for recovery functions
+    /// @notice prevents burn for transfer functions
     /// @dev checks for zero address and reverts if true
-    /// @param _recipient address of the recovery recipient
+    /// @param _recipient address of the transfer recipient
     modifier burnDisallowed(address _recipient) {
         require(_recipient != address(0), "NO BURN");
         _;
     }
 
-    /// @notice prevents `owner` from renouncing ownership and potentially locking assets forever
+    /// @notice forbids owner from renouncing ownership and locking assets forever
     /// @dev overrides Ownable's `renounceOwnership` to always revert
     function renounceOwnership() public pure override {
         revert("DISABLED");
     }
 
     /// @notice transfers ether from this contract
-    /// @dev using the safer `call` instead of `transfer`
+    /// @dev using `address.call` is safer to transfer to other contracts
     /// @param _recipient address to transfer ether to
     /// @param _amount amount of ether to transfer
     function transferEther(address _recipient, uint256 _amount)
@@ -67,7 +67,7 @@ contract InsuranceFund is Ownable {
     }
 
     /// @notice transfer an ERC20 token from this contract
-    /// @dev SafeERC20.safeTransfer doesn't return a bool as it performs an internal `require` check
+    /// @dev `SafeERC20.safeTransfer` doesn't always return a bool as it performs an internal `require` check
     /// @param _token address of the ERC20 token
     /// @param _recipient address to transfer the tokens to
     /// @param _amount amount of tokens to transfer
@@ -81,10 +81,11 @@ contract InsuranceFund is Ownable {
     }
 
     /// @notice transfer an ERC721 token from this contract
-    /// @dev IERC721.safeTransferFrom doesn't return a bool as it performs an internal `require` check
+    /// @dev `IERC721.safeTransferFrom` doesn't always return a bool as it performs an internal `require` check
     /// @param _token address of the ERC721 token
-    /// @param _tokenId id of the individual token
     /// @param _recipient address to transfer the token to
+    /// @param _tokenId id of the individual token
+    /// @param _data data to transfer along; required for `ERC721Receiver` recipients
     function transferERC721(
         address _token,
         address _recipient,
@@ -100,11 +101,13 @@ contract InsuranceFund is Ownable {
         emit ERC721Transferred(_token, _recipient, _tokenId, _data);
     }
 
-    /// @notice recover an ERC1155 token on this contract's balance as the owner
-    /// @dev IERC1155.safeTransferFrom doesn't return a bool as it performs an internal `require` check
+    /// @notice transfer an ERC1155 token from this contract
+    /// @dev `IERC1155.safeTransferFrom` doesn't always return a bool as it performs an internal `require` check
     /// @param _token address of the ERC1155 token that is being recovered
-    /// @param _tokenId id of the individual token to transfer
     /// @param _recipient address to transfer the token to
+    /// @param _tokenId id of the individual token to transfer
+    /// @param _amount amount of tokens to transfer
+    /// @param _data data to transfer along; required for `ERC721Receiver` recipients
     function transferERC1155(
         address _token,
         address _recipient,
