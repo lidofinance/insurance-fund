2f484ec509e25508d0800b81aec776b3e09af33e
Azat Serikov <azatsdev@gmail.com>
2022-09-23 12:21:04 +0600

    fix: use transferShares

--- scripts/check.py
+++ scripts/check.py
@@ -7,61 +7,72 @@ from utils.env import check_env_var
 import utils.log as log
 import utils.config as config
 
+
 def main():
-    if ("fork" not in brownie.network.show_active()):
+    if "fork" not in brownie.network.show_active():
         log.error("Active network is not a fork!")
         sys.exit()
 
     INSURANCE_FUND = check_env_var("INSURANCE_FUND")
     INSURANCE_WSTETH = check_env_var("INSURANCE_WSTETH")
-    
-    log.info("Initializing `InsuranceFund` at", INSURANCE_FUND)
-    insurance_fund = brownie.interface.InsuranceFund(INSURANCE_FUND)
 
-    owner_address = insurance_fund.owner()
-    log.info("Owner", owner_address)
+    log.info("Initializing Insurance Fund...")
+    insurance_fund = brownie.interface.InsuranceFund(INSURANCE_FUND)
+    log.okay("Insurance Fund", insurance_fund.address)
     log.proceed()
 
     log.info("Taking over owner account...")
-    owner = brownie.accounts.at(owner_address, True)
-
-    log.info("Initializing stETH...")
-    steth_address = config.STETH[brownie.network.chain.id][0]
-    steth = brownie.interface.stETH(steth_address)
-
-    log.info("Initializing wstETH...")
-    wsteth_address = config.WSTETH[brownie.network.chain.id][0]
-    wsteth = brownie.interface.wstETH(wsteth_address)
-
-    wsteth_amount = eth_utils.to_wei(INSURANCE_WSTETH, 'ether')
-    equivalent_steth_amount = wsteth.getStETHByWstETH(wsteth_amount)
-    log.info(f"{wsteth_amount} wstETH equals {equivalent_steth_amount} stETH.")
+    owner = brownie.accounts.at(insurance_fund.owner(), True)
+    log.okay("Owner", owner.address)
     log.proceed()
 
-    log.info(f"Approving {equivalent_steth_amount} stETH to wstETH contract...")
-    steth.approve(wsteth.address, equivalent_steth_amount, {"from": owner})
+    log.info("Initializing stETH contract...")
+    steth = brownie.interface.stETH(config.STETH[brownie.network.chain.id][0])
+    log.okay("stETH", steth.address)
+    log.proceed()
 
-    prev_owner_balance = wsteth.balanceOf(owner.address)
+    log.info(
+        f"Transferring {INSURANCE_WSTETH} wstETH to the fund using `transferShares`..."
+    )
+    shares_to_transfer = float(INSURANCE_WSTETH) * 10 ** steth.decimals()
+    sender_shares = steth.sharesOf(owner.address)
+    recipient_shares = steth.sharesOf(insurance_fund.address)
+    log.info("Sender", owner.address)
+    log.info("Sender shares", sender_shares)
+    log.info("Recipient", insurance_fund.address)
+    log.info("Recipient shares", recipient_shares)
+    log.info("Shares to transfer", shares_to_transfer)
+    log.proceed()
 
-    log.info("stETH balance", steth.balanceOf(owner.address))
-    log.info(f"Wrapping {equivalent_steth_amount} sTETH...")
-    wsteth.wrap(equivalent_steth_amount, {"from": owner})
+    steth.transferShares(insurance_fund.address, shares_to_transfer, {"from": owner})
+    assert steth.sharesOf(owner.address) == sender_shares - shares_to_transfer
+    assert (
+        steth.sharesOf(insurance_fund.address) == recipient_shares + shares_to_transfer
+    )
+    log.okay("Transfer succesful!")
+    log.info("Sender shares", steth.sharesOf(owner.address))
+    log.info("Recipient shares", steth.sharesOf(insurance_fund.address))
+    log.proceed()
 
-    assert math.isclose(wsteth.balanceOf(owner.address), prev_owner_balance + wsteth_amount, abs_tol=config.STETH_ERROR_MARGIN)
+    log.info(f"Recovering all stETH back from the fund...")
+    steth_amount = steth.balanceOf(insurance_fund.address)
+    recipient_balance = steth.balanceOf(owner.address)
+    log.info("Method", "transferERC20")
+    log.info("Recipient", owner.address)
+    log.info("StETH to recover", steth_amount)
+    log.proceed()
 
-    prev_fund_balance = wsteth.balanceOf(insurance_fund.address)
-    corrected_wsteth_amount = wsteth.getWstETHByStETH(equivalent_steth_amount)
+    insurance_fund.transferERC20(
+        steth.address, owner.address, steth_amount, {"from": owner}
+    )
+    assert math.isclose(
+        steth.balanceOf(owner.address),
+        recipient_balance + steth_amount,
+        abs_tol=config.STETH_ERROR_MARGIN,
+    )
 
-    log.info(f"Sending {wsteth_amount} wsTETH to the fund...")
-    wsteth.transfer(insurance_fund.address, corrected_wsteth_amount, {"from": owner})
-
-    assert math.isclose(wsteth.balanceOf(insurance_fund.address), prev_fund_balance + corrected_wsteth_amount, abs_tol=config.STETH_ERROR_MARGIN)
-
-    prev_owner_balance = wsteth.balanceOf(owner.address)
-    log.info(f"Retrieving {corrected_wsteth_amount} from the fund...")
-    insurance_fund.transferERC20(wsteth.address, owner.address, corrected_wsteth_amount, {"from": owner})
-
-    assert math.isclose(wsteth.balanceOf(owner.address), prev_owner_balance + corrected_wsteth_amount, abs_tol=config.STETH_ERROR_MARGIN)
+    log.okay("Recover successful!")
+    log.info("Insurance Fund shares", steth.sharesOf(insurance_fund.address))
+    log.info("Recipient shares", steth.sharesOf(owner.address))
 
     log.okay("Check successful!")
-
