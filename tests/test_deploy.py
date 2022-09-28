6f1f7326f71b774867d69232e97f566fb5dc8ed8
Azat Serikov <azatsdev@gmail.com>
2022-09-28 15:53:40 +0600

    test: check deploy events

--- tests/test_deploy.py
+++ tests/test_deploy.py
@@ -3,6 +3,18 @@ import brownie
 from utils.config import AGENT
 
 
+def test_deploy_events(deployer, owner):
+    tx = brownie.InsuranceFund.deploy(owner.address, {"from": deployer}).tx
+    assert "OwnershipTransferred" in tx.events
+    assert (
+        len(tx.events["OwnershipTransferred"]) == 2
+    ), "there should be 2 ownership transfers"
+    assert tx.events["OwnershipTransferred"][0]["previousOwner"] == brownie.ZERO_ADDRESS
+    assert tx.events["OwnershipTransferred"][0]["newOwner"] == deployer.address
+    assert tx.events["OwnershipTransferred"][1]["previousOwner"] == deployer.address
+    assert tx.events["OwnershipTransferred"][1]["newOwner"] == owner.address
+
+
 def test_deploy_with_zero_address(owner):
     with brownie.reverts("ZERO ADDRESS"):
         brownie.InsuranceFund.deploy(brownie.ZERO_ADDRESS, {"from": owner})
